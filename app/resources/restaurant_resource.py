fa8a363e6ed2d965af118f059046d03b6fd40206
alyucu6 <allisonycui@gmail.com>
2024-10-29 18:13:37 -0400

    changed back ip

--- app/resources/restaurant_resource.py
+++ app/resources/restaurant_resource.py
@@ -18,11 +18,6 @@ class RestaurantResource:
             'password': 'dbuserdbuser',
             'database': 'availability',
             'port': 3306
-            # 'host': os.getenv('DB_HOST'),
-            # 'user': os.getenv('DB_USER'),
-            # 'password': os.getenv('DB_PASSWORD'),
-            # 'database': os.getenv('DB_NAME'),
-            # 'port': int(os.getenv('DB_PORT', 3306))
         }
 
     def get_db_connection(self):
